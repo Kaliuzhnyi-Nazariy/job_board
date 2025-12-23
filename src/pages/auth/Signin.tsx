28c0c32dec347f0fe1231269b53859ba8eaacfc8
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-12-23 14:22:48 +0100

    signin page

---
+++ src/pages/auth/Signin.tsx
@@ -0,0 +1,72 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { Link } from "react-router";
+import z from "zod";
+
+type SignIn = {
+  email: string;
+  password: string;
+};
+
+const signinState: SignIn = {
+  email: "",
+  password: "",
+};
+
+const zodValidation = z.object({
+  email: z.email({ pattern: z.regexes.email }),
+  password: z
+    .string()
+    .min(6, "Password should be at least 6 characters")
+    .max(16, "Password must be mazimum 16 characters"),
+});
+
+const Signin = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: signinState,
+    resolver: zodResolver(zodValidation),
+  });
+
+  const onSubmit: SubmitHandler<SignIn> = (data) => {
+    console.log(data);
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="">
+        <div className="">
+          <h1>Sign in</h1>
+          <p>
+            Don't have account <Link to="/auth/signup">Create account</Link>
+          </p>
+        </div>
+      </div>
+      <input type="text" {...register("email")} placeholder="Emaill address" />
+      <p>{errors.email?.message}</p>
+
+      <div className="relative">
+        <input
+          type={showPassword ? "text" : "password"}
+          {...register("password")}
+          placeholder="Password"
+        />
+        <button type="button" onClick={() => setShowPassword(!showPassword)}>
+          eye
+        </button>
+      </div>
+      <p>{errors.password?.message}</p>
+      <Link to="/forget-password">Forget password</Link>
+
+      <button type="submit">Create account (right arrow)</button>
+    </form>
+  );
+};
+
+export default Signin;
