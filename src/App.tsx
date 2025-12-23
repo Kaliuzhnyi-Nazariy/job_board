fcfaa7c9b0707b311dcb15be8f44d0a534a0a305
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-12-23 14:22:04 +0100

    add page router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router";
+import Greeting from "./pages";
+import { lazy } from "react";
+
+const SigninPage = lazy(() => import("./pages/auth/Signin"));
+const SignupPage = lazy(() => import("./pages/auth/Signup"));
+const ForgetPage = lazy(() => import("./pages/auth/ForgetPassword"));
+const ResetPasswordPage = lazy(() => import("./pages/auth/ResetPassword"));
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Greeting />} />
+      <Route path="/auth/signin" element={<SigninPage />} />
+      <Route path="/auth/signup" element={<SignupPage />} />
+      <Route path="/forget-password" element={<ForgetPage />} />
+      <Route path="/reset-password" element={<ResetPasswordPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
