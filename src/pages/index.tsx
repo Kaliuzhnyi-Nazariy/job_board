7f69a836a08aa4b23841c335b46f38bc1ae95187
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-12-23 14:22:16 +0100

    greeting page

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Greeting = () => {
+  return (
+    <div>
+      <h1>Greeting</h1>
+      <div style={{ display: "flex", gap: "12px" }}>
+        <Link to={"/auth/signin"}>Sign in</Link>
+        <Link to={"/auth/signup"}>Start now</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Greeting;
