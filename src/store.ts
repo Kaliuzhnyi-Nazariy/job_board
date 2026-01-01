47db3f2bf60363b31581662844823c28803028ce
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-01 20:08:22 +0100

    add user reducer to store

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "../features/auth/authSlice";
+import { userReducer } from "../features/user/userSlice";
 
 const store = configureStore({
   reducer: {
-    user: authReducer,
+    auth: authReducer,
+    user: userReducer,
   },
 });
 
