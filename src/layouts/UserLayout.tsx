d7b261ecfe4c37b5aff8fb8c5610800263068794
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-01 20:12:34 +0100

    get user layout

---
+++ src/layouts/UserLayout.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { Outlet } from "react-router";
+import { useAppDispatch } from "../../features/hooks/dispatchHook";
+import { getMe } from "../../features/user/userRequest";
+import Navbar from "../Components/Navbar";
+import Header from "../Components/header/Header";
+
+const UserLayout = () => {
+  // fetch user
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getMe());
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default UserLayout;
