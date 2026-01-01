0c184658dc06ce5e9c573ca1b999e71a043730cf
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-01 20:13:15 +0100

    add pages and wrapper some pages in role-guard-layers

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,16 @@ const SignupPage = lazy(() => import("./pages/auth/Signup"));
 const ForgetPage = lazy(() => import("./pages/auth/ForgetPassword"));
 const ResetPasswordPage = lazy(() => import("./pages/auth/ResetPassword"));
 
+const UserLayoutComponent = lazy(() => import("./layouts/UserLayout"));
+const CandidateRouteComponent = lazy(() => import("./layouts/CandidateRoute"));
+const EmployerRouteComponent = lazy(() => import("./layouts/EmployerRoute"));
+
 const HomeCandidate = lazy(() => import("./pages/candidate/home"));
+const FindJob = lazy(() => import("./pages/candidate/FindJob"));
+
+const HomeEmployer = lazy(() => import("./pages/employer/home"));
+
+const Forbidden = lazy(() => import("./pages/error/Forbidden"));
 
 function App() {
   return (
@@ -17,7 +26,16 @@ function App() {
       <Route path="/auth/signup" element={<SignupPage />} />
       <Route path="/forget-password" element={<ForgetPage />} />
       <Route path="/reset-password/*" element={<ResetPasswordPage />} />
-      <Route path="/candidate/home" element={<HomeCandidate />} />
+      <Route path="/" element={<UserLayoutComponent />}>
+        <Route element={<CandidateRouteComponent />}>
+          <Route path="/candidate/home" element={<HomeCandidate />} />
+          <Route path="/candidate/find-job" element={<FindJob />} />
+        </Route>
+        <Route element={<EmployerRouteComponent />}>
+          <Route path="/employer/home" element={<HomeEmployer />} />
+        </Route>
+      </Route>
+      <Route path="/forbidden" element={<Forbidden />} />
     </Routes>
   );
 }
