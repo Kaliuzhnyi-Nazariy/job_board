298f0efc40f49fb936bdf0fe6fda49b8e554c97e
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-01 20:10:42 +0100

    header for app

---
+++ src/Components/header/Header.tsx
@@ -0,0 +1,12 @@
+import Searchbar from "./Searchbar";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>MyJob</h1>
+      <Searchbar />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Components/header/Searchbar.tsx
@@ -0,0 +1,90 @@
+// import { countries } from "../../extras/countries";
+// import TextField from "@mui/material/TextField";
+// import InputAdornment from "@mui/material/InputAdornment";
+// import SearchIcon from "@mui/icons-material/Search";
+
+// const Searchbar = () => {
+//   console.log(countries.map);
+//   return (
+//     <div className="">
+//       <select>
+//         {countries.map((country) => {
+//           return <option value={country}>{country}</option>;
+//         })}
+//       </select>
+//       {/* <input type="text" /> */}
+//       <TextField
+//         id="outlined-basic"
+//         InputProps={{
+//           startAdornment: (
+//             <InputAdornment position="start">
+//               <SearchIcon />
+//             </InputAdornment>
+//           ),
+//         }}
+//         variant="outlined"
+//       ></TextField>
+//       <div className="size-6 bg-purple-500"></div>
+//       <div className="size-12 bg-purple-500"></div>
+//     </div>
+//   );
+// };
+
+// export default Searchbar;
+
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import MenuItem from "@mui/material/MenuItem";
+import SearchIcon from "@mui/icons-material/Search";
+import Box from "@mui/material/Box";
+import { countries } from "../../extras/countries";
+
+const Searchbar = () => {
+  return (
+    <Box sx={{ width: "100%", maxWidth: 700 }}>
+      <TextField
+        fullWidth
+        placeholder="Job title, keyword, company"
+        variant="outlined"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              {/* Country select */}
+              <TextField
+                select
+                variant="standard"
+                defaultValue="Poland"
+                InputProps={{ disableUnderline: true }}
+                sx={{
+                  minWidth: 100,
+                  mr: 1,
+                }}
+              >
+                {countries.map((country) => (
+                  <MenuItem key={country} value={country}>
+                    {country}
+                  </MenuItem>
+                ))}
+              </TextField>
+
+              {/* Divider */}
+              <Box
+                sx={{
+                  height: 24,
+                  width: "1px",
+                  bgcolor: "divider",
+                  mx: 1,
+                }}
+              />
+
+              {/* Search icon */}
+              <SearchIcon sx={{ color: "primary.main" }} />
+            </InputAdornment>
+          ),
+        }}
+      />
+    </Box>
+  );
+};
+
+export default Searchbar;
