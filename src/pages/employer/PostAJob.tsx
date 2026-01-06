8b0b5533d01e668eab9e4b40617a868f42141d22
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-06 20:09:14 +0100

    changed types and some inputs where enum is type add query

--- src/pages/employer/PostAJob.tsx
+++ src/pages/employer/PostAJob.tsx
@@ -1,17 +1,20 @@
+// TO-DO:
+// error handling from tanstack
+
 import { useForm, type SubmitHandler } from "react-hook-form";
 import { type IJobForm } from "../../../features/job/interfaces";
-import { useAppDispatch } from "../../../features/hooks/dispatchHook";
-import { postJob } from "../../../features/job/jobRequest";
+import { postJob } from "../../../features/job/jobRequests";
 import z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
 
 const PostAJob = () => {
   const JobForm: IJobForm = {
     title: "",
     position: "",
     description: "",
-    minSalary: "",
-    maxSalary: "",
+    minSalary: 0,
+    maxSalary: 0,
     salaryType: "month",
     workTime: "full_time",
     location: "",
@@ -20,31 +23,36 @@ const PostAJob = () => {
     experience: "",
   };
 
-  const jobValidation = z.object({
-    title: z.string().min(3, "The title should be longer than 3 symbols!"),
-    position: z.string(),
-    description: z
-      .string()
-      .min(20, "Description should be at least 20 charachters long!")
-      .max(1028, "Description shouldn't be longer than 1028 characters!"),
-    minSalary: z.string(),
-    maxSalary: z.string(),
-    salaryType: z.enum(
-      ["month", "week", "year", "contract"],
-      'That field can be filled only with that values: "month", "week", "year", "contract"'
-    ),
-    workTime: z.enum(
-      ["full_time", "part_time", "internship", "contract"],
-      "That field can be filled only with that values: 'full_time', 'part_time', 'internship', 'contract' "
-    ),
-    location: z.string(),
-    education: z.string().optional(),
-    responsobilities: z
-      .string()
-      .max(1028, "Responsobilities shouldn't be longer than 1028 characters!")
-      .optional(),
-    experience: z.string().optional(),
-  });
+  const jobValidation = z
+    .object({
+      title: z.string().min(3, "The title should be longer than 3 symbols!"),
+      position: z.string(),
+      description: z
+        .string()
+        .min(20, "Description should be at least 20 charachters long!")
+        .max(1028, "Description shouldn't be longer than 1028 characters!"),
+      minSalary: z.number().gt(-1, "The value cannot be negative!"),
+      maxSalary: z.number().gt(-1, "The value cannot be negative!"),
+      salaryType: z.enum(
+        ["month", "week", "hour", "year", "contract"],
+        'That field can be filled only with that values: "month", "week", "year", "contract"'
+      ),
+      workTime: z.enum(
+        ["full_time", "part_time", "internship", "contract"],
+        "That field can be filled only with that values: 'full_time', 'part_time', 'internship', 'contract' "
+      ),
+      location: z.string(),
+      education: z.string().optional(),
+      responsobilities: z
+        .string()
+        .max(1028, "Responsobilities shouldn't be longer than 1028 characters!")
+        .optional(),
+      experience: z.string().optional(),
+    })
+    .refine((data) => data.maxSalary >= data.minSalary, {
+      message: "Maximum salary should be higher or equal to minimal salary",
+      path: ["maxSalary"],
+    });
 
   const {
     register,
@@ -55,12 +63,12 @@ const PostAJob = () => {
     resolver: zodResolver(jobValidation),
   });
 
-  const dispatch = useAppDispatch();
+  const mutation = useMutation({
+    mutationFn: (data: IJobForm) => postJob(data),
+  });
 
   const submitHandler: SubmitHandler<IJobForm> = async (data) => {
-    // console.log(data);
-    if (data.maxSalary < data.minSalary) return;
-    await dispatch(postJob(data));
+    mutation.mutate(data);
   };
 
   console.log({ errors });
@@ -80,15 +88,28 @@ const PostAJob = () => {
 
         <div className={inputBlock}>
           <label>Min Salary</label>
-          <input type="number" {...register("minSalary")} />
+          <input
+            type="number"
+            {...register("minSalary", { valueAsNumber: true })}
+          />
         </div>
         <div className={inputBlock}>
           <label>Max Salary</label>
-          <input type="number" {...register("maxSalary")} />
+          <input
+            type="number"
+            {...register("maxSalary", { valueAsNumber: true })}
+          />
         </div>
         <div className={inputBlock}>
           <label>Salary Type</label>
-          <input type="text" {...register("salaryType")} />
+          {/* <input type="text" {...register("salaryType")} /> */}
+          <select {...register("salaryType")}>
+            <option value="month">month</option>
+            <option value="week">week</option>
+            <option value="hour">hour</option>
+            <option value="year">year</option>
+            <option value="contract">contract</option>
+          </select>
         </div>
 
         <h3>Advance Information</h3>
@@ -104,7 +125,13 @@ const PostAJob = () => {
           </div>
           <div className={inputBlock}>
             <label>Job Type</label>
-            <input type="text" {...register("workTime")} />
+            {/* <input type="text" {...register("workTime")} /> */}
+            <select {...register("workTime")}>
+              <option value="full_time">Full-time</option>
+              <option value="part_time">Part-time</option>
+              <option value="internship">internship</option>
+              <option value="contract">contract</option>
+            </select>
           </div>
           <div className={inputBlock}>
             <label>Job level</label>
@@ -128,7 +155,9 @@ const PostAJob = () => {
           <textarea {...register("responsobilities")} />
         </div>
 
-        <button>Post Job</button>
+        <button disabled={mutation.isPending} className="disabled:opacity-50">
+          Post Job
+        </button>
       </form>
     </div>
   );
