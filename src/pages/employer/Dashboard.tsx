a962da3658c29e286f4d7a94b33f7cf8414b91cd
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-06 10:27:52 +0100

    dashboard layout-component

---
+++ src/pages/employer/Dashboard.tsx
@@ -0,0 +1,42 @@
+import { Link, Outlet } from "react-router";
+
+const Dashboard = () => {
+  const baseURL = "/employer/dashboard";
+  return (
+    <div className="flex gap-5">
+      <div>
+        <h4 className="uppercase">employers Dashboard</h4>
+        <ul>
+          <li>
+            <Link to={baseURL}>Overview</Link>
+          </li>
+          <li>
+            <Link to="#">Employer Profile</Link>
+          </li>
+          <li>
+            <Link to={`${baseURL}/post-a-job`}>Post a Job</Link>
+          </li>
+          <li>
+            <Link to="#">My Jobs</Link>
+          </li>
+          <li>
+            <Link to="#">Saved Candidates</Link>
+          </li>
+          <li>
+            <Link to="#">Plans & Billing</Link>
+          </li>
+          <li>
+            <Link to="#">All Companies</Link>
+          </li>
+          <li>
+            <Link to="#">Settings</Link>
+          </li>
+        </ul>
+      </div>
+
+      <Outlet />
+    </div>
+  );
+};
+
+export default Dashboard;
