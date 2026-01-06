5434781c9e509d53c77db59373ab5fe008eb68a9
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-06 10:27:26 +0100

    overview page, just to check links

---
+++ src/pages/employer/Overview.tsx
@@ -0,0 +1,17 @@
+const Overview = () => {
+  return (
+    <div>
+      <h1>Hello, instagram</h1>
+      <span>
+        <p>589</p>
+        <p>open jobs</p>
+      </span>
+      <span>
+        <p>2, 517</p>
+        <p>saved candidates</p>
+      </span>
+    </div>
+  );
+};
+
+export default Overview;
