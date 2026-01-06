865520f4d82de17c1c8117d362582773dde19219
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-06 20:07:53 +0100

    delete job reducer

--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "../features/auth/authSlice";
 import { userReducer } from "../features/user/userSlice";
-import { jobReducer } from "../features/job/jobSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
-    job: jobReducer,
   },
 });
 
