bb0d22e60ee62e7bc97d7c82c0a34dc3e93ba20d
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-07 16:48:31 +0100

    add selector for error

--- features/user/userSelector.ts
+++ features/user/userSelector.ts
@@ -3,3 +3,4 @@ import { type StoreState } from "../../src/store";
 export const userRole = (state: StoreState) => state.user.user?.role;
 export const userLoading = (state: StoreState) => state.user.isLoading;
 export const userInitialized = (state: StoreState) => state.user.initialized;
+export const userError = (state: StoreState) => state.user.isError;
