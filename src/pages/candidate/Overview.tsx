923a9a72601bac1e2f3cd0d79b7bde9eece7b8c5
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-09 11:48:47 +0100

    create page for testing the candidate navbar

---
+++ src/pages/candidate/Overview.tsx
@@ -0,0 +1,10 @@
+const Overview = () => {
+  return (
+    <div>
+      <h1>Hello, Ester Howard</h1>
+      <small>Here is your daily activities and job alerts</small>
+    </div>
+  );
+};
+
+export default Overview;
