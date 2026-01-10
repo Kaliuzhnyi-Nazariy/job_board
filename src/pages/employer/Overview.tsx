b023b978557f28bfee608810b24c8c3d9a71224f
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-10 19:59:22 +0100

    changed from mocks to real values

--- src/pages/employer/Overview.tsx
+++ src/pages/employer/Overview.tsx
@@ -1,14 +1,29 @@
+import { useSelector } from "react-redux";
+import { username } from "../../../features/user/userSelector";
+import { useQuery } from "@tanstack/react-query";
+import { getMyJobs } from "../../../features/job/jobRequests";
+
 const Overview = () => {
+  const usernameValue = useSelector(username);
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["myjobs"],
+    queryFn: getMyJobs,
+  });
+
   return (
     <div>
-      <h1>Hello, instagram</h1>
+      <h1>Hello, {usernameValue}</h1>
       <span>
-        <p>589</p>
-        <p>open jobs</p>
+        <p>
+          {isError
+            ? "Error ocured!"
+            : isLoading
+            ? "Loading"
+            : `${data.job.length} jobs`}
+        </p>
       </span>
       <span>
-        <p>2, 517</p>
-        <p>saved candidates</p>
+        <p>2, 517 saved candidates</p>
       </span>
     </div>
   );
