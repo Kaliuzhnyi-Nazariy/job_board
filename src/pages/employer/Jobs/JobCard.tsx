6df5c2b58898fb7549a2f13a5e4e4599eda86abd
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-10 19:59:01 +0100

    add default value for bio

---
+++ src/pages/employer/Jobs/JobCard.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import type { IJob } from "../../../../features/job/interfaces";
+import { Link } from "react-router";
+
+const JobCard = ({ data }: { data: IJob }) => {
+  return (
+    <Link to="#" className="flex p-5 justify-between w-full">
+      <div className="">
+        <h2>{data.title}</h2>
+        <p className="opacity-50">{data.work_time}</p>
+      </div>
+
+      <div className="">.</div>
+    </Link>
+  );
+};
+
+export default JobCard;
