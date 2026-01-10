701c160ff8fd3c9cfd1b2ff958acb867ad65b0d7
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-10 19:58:38 +0100

    add username selector

--- features/user/userSelector.ts
+++ features/user/userSelector.ts
@@ -4,3 +4,4 @@ export const userRole = (state: StoreState) => state.user.user?.role;
 export const userLoading = (state: StoreState) => state.user.isLoading;
 export const userInitialized = (state: StoreState) => state.user.initialized;
 export const userError = (state: StoreState) => state.user.isError;
+export const username = (state: StoreState) => state.user.user?.username;
