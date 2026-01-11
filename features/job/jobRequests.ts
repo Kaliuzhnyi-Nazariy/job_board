e972110e8f4cf9259f54e520dc5377877ee75ba6
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-11 21:29:32 +0100

    add job update request

--- features/job/jobRequests.ts
+++ features/job/jobRequests.ts
@@ -1,4 +1,8 @@
-import type { IGetJob, IJobForm } from "./interfaces";
+import type {
+  IGetJob,
+  IJobForm,
+  // IJobFormUpdate
+} from "./interfaces";
 import api from "../api/api";
 
 export const postJob = async (data: IJobForm) => {
@@ -26,6 +30,22 @@ export const getMyJobs = async () => {
 
 export const getMyJob = async (jobId: string) => {
   const res = await api.get("/job/my-jobs/" + jobId);
+
+  return res.data;
+};
+
+export const updateMyJob = async ({
+  data,
+  jobId,
+}: {
+  data: IJobForm;
+  jobId: string;
+}) => {
+  const salary = `$${data.minSalary}-$${data.maxSalary}/${data.salaryType}`;
+  const res = await api.put(`/job/update/${jobId}`, {
+    ...data,
+    salary,
+  });
 
   return res.data;
 };
