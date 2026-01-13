5ce2d0972b2e1a4dc83512923e4bbbf4dbea04a5
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-13 14:37:59 +0100

    add selectors

--- features/user/userSelector.ts
+++ features/user/userSelector.ts
@@ -5,3 +5,5 @@ export const userLoading = (state: StoreState) => state.user.isLoading;
 export const userInitialized = (state: StoreState) => state.user.initialized;
 export const userError = (state: StoreState) => state.user.isError;
 export const username = (state: StoreState) => state.user.user?.username;
+export const userFullName = (state: StoreState) => state.user.user?.full_name;
+export const userId = (state: StoreState) => state.user.user?.id;
