54039e2a5bd13da95ed5f1e7b70a92d4770ab7d6
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-19 11:13:49 +0100

    add modal with application details

---
+++ src/Components/modals/ApplicationCandidateDetails.tsx
@@ -0,0 +1,165 @@
+import { Box, Modal } from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
+import { getApplicantDetails } from "../../../features/application/applicationRequest";
+import { Link } from "react-router";
+
+const ApplicationCandidateDetails = ({
+  open,
+  handleClose,
+  applicationId,
+  jobId,
+}: {
+  open: boolean;
+  handleClose: () => void;
+  applicationId: string;
+  jobId: string;
+}) => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["getCandidateDetails", applicationId],
+    queryFn: () => getApplicantDetails(jobId, applicationId),
+    enabled: open && !!applicationId && !!jobId,
+  });
+
+  if (!open) return null;
+
+  return (
+    <Modal open={open} onClose={handleClose}>
+      <Box
+        sx={{
+          backgroundColor: "#fff",
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          p: 2.5,
+          borderRadius: "12px",
+          width: "80%",
+        }}
+      >
+        {/* <h2>Applicant data</h2> */}
+        {/* <p>{applicationId}</p>
+        <p>{jobId}</p> */}
+        {isLoading || !data ? (
+          "Loading"
+        ) : (
+          <>
+            <div className="flex items-center justify-between">
+              <div className="flex gap-6 items-center">
+                <div className="size-20 rounded-full bg-gray-500"></div>
+                <div className="flex flex-col gap-2">
+                  <h3>{data.full_name}</h3>
+                  <span className="opacity-50">{data.speciality}</span>
+                </div>
+              </div>
+              <ul className="flex gap-3">
+                <li>
+                  <Link to={`mailto:` + data.email}>Send Mail</Link>
+                </li>
+                <li>
+                  <button>Hire Candidate</button>
+                </li>
+              </ul>
+            </div>
+            <div className="grid grid-cols-2 g-18">
+              <div className="">
+                <>
+                  <h3>Biography</h3>
+                  <article>{data.biography || "No biography"}</article>
+                </>
+
+                <>
+                  <h3>Cover letter</h3>
+                  <article>{data.covering_letter || "No letter"}</article>
+                </>
+              </div>
+
+              <div className="">
+                <div className="grid grid-cols-2 gap-x-4 gap-y-6">
+                  <div className="">
+                    <div className="size-6 bg-blue-500"></div>
+                    <h5>DATE OF BIRTH</h5>
+                    <p className="text-[12px]">{data.date_of_birth}</p>
+                  </div>
+
+                  <div className="">
+                    <div className="size-6 bg-blue-500"></div>
+                    <h5>gender</h5>
+                    <p className="text-[12px]">{data.gender}</p>
+                  </div>
+
+                  <div className="">
+                    <div className="size-6 bg-blue-500"></div>
+                    <h5>experience</h5>
+                    <p className="text-[12px]">{data.experience}</p>
+                  </div>
+
+                  <div className="">
+                    <div className="size-6 bg-blue-500"></div>
+                    <h5>education</h5>
+                    <p className="text-[12px]">{data.education}</p>
+                  </div>
+                </div>
+
+                <div className="">
+                  <h3>Contact Information</h3>
+                  <ul>
+                    {data.website && (
+                      <li>
+                        <div className="flex gap-4">
+                          <div className="size-8 rounded-full bg-blue-500"></div>
+                          <div>
+                            <h5>WEBSite</h5>
+                            <Link to={data.website}>{data.website}</Link>
+                          </div>
+                        </div>
+                      </li>
+                    )}
+                    {data.location && (
+                      <li>
+                        <div className="flex gap-4">
+                          <div className="size-8 rounded-full bg-blue-500"></div>
+                          <div>
+                            <h5>location</h5>
+                            <Link to={data.location}>{data.location}</Link>
+                          </div>
+                        </div>
+                      </li>
+                    )}
+                    {data.phone && (
+                      <li>
+                        <div className="flex gap-4">
+                          <div className="size-8 rounded-full bg-blue-500"></div>
+                          <div>
+                            <h5>phone</h5>
+                            <Link to={data.phone}>{data.phone}</Link>
+                          </div>
+                        </div>
+                      </li>
+                    )}
+                    <li>
+                      <div className="flex gap-4">
+                        <div className="size-8 rounded-full bg-blue-500"></div>
+                        <div>
+                          <h5>Email Address</h5>
+                          <Link to={`mailto:${data.website}`}>
+                            {data.email}
+                          </Link>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="w-full flex justify-around items-center">
+              <button className="cursor-pointer">Reject</button>
+              <button className="cursor-pointer">Hire</button>
+            </div>
+          </>
+        )}
+      </Box>
+    </Modal>
+  );
+};
+
+export default ApplicationCandidateDetails;
