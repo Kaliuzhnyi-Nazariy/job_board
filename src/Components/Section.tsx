4a0fa587ca8dae103b7565baff75cf71287e3e98
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-29 14:24:34 +0100

    create a component which will remove repeated code

---
+++ src/Components/Section.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Section = ({
+  children,
+  extraStyles,
+}: {
+  children: React.ReactNode;
+  extraStyles?: string;
+}) => {
+  return (
+    <section className={`px-75 ${extraStyles && extraStyles}`}>
+      {children}
+    </section>
+  );
+};
+
+export default Section;
