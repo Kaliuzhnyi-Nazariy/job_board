3ef514fe5fbc9574ee87068e3694ab60a0052aec
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-30 10:50:41 +0100

    styles for job list view and filtering

--- src/Components/findJob/List/filter/VacanciesView.tsx
+++ src/Components/findJob/List/filter/VacanciesView.tsx
@@ -1,10 +1,16 @@
+import { MenuItem, Select } from "@mui/material";
 import { useSearchParams } from "react-router";
 
+import ViewCompactIcon from "@mui/icons-material/ViewCompact";
+import ViewAgendaIcon from "@mui/icons-material/ViewAgenda";
+
 const VacanciesView = ({
+  listView,
   setView,
 }: // setJobNumberOnPage,
 // setSortingType,
 {
+  listView: "grid" | "list";
   setJobNumberOnPage: (number: 12 | 16) => void;
   setView: (view: "grid" | "list") => void;
   setSortingType: (type: "oldest" | "newest") => void;
@@ -12,50 +18,77 @@ const VacanciesView = ({
   const [searchParams, setSearchParams] = useSearchParams();
 
   return (
-    <div className="flex gap-4">
-      <select
-        defaultValue={searchParams.get("order") || "oldest"}
+    <div className="flex gap-4 items-center">
+      <Select
+        // id="demo-simple-select"
+        value={searchParams.get("order") || "oldest"}
         onChange={(select) =>
           setSearchParams((prev) => {
             const params = new URLSearchParams(prev);
-            params.set("order", select.target.value);
+            params.set("order", select.target.value as string);
             return params;
           })
         }
+        className="w-45 h-12"
+        sx={{
+          "& .MuiOutlinedInput-root": {
+            height: 48,
+          },
+          "& .MuiOutlinedInput-input": {
+            padding: "14px 16px 14px 18px",
+          },
+        }}
       >
-        <option value="oldest">Latest</option>
-        <option value="newest">Earlier</option>
-      </select>
-      <select
+        <MenuItem value="oldest">Latest</MenuItem>
+        <MenuItem value="newest">Earlier</MenuItem>
+      </Select>
+      <Select
+        // id="demo-simple-select"
         defaultValue={searchParams.get("limit") || 12}
         onChange={(val) =>
           setSearchParams((prev) => {
             const params = new URLSearchParams(prev);
-            params.set("limit", val.target.value);
+            params.set("limit", val.target.value as "12" | "16");
             return params;
           })
         }
+        className="w-45 h-12"
+        sx={{
+          "& .MuiOutlinedInput-root": {
+            height: 48,
+          },
+          "& .MuiOutlinedInput-input": {
+            padding: "14px 16px 14px 18px",
+          },
+        }}
       >
-        <option value="12">12 per page</option>
-        <option value="16">16 per page</option>
-      </select>
-      <ul className="flex gap-2">
-        <li>
+        <MenuItem value="12">12 per page</MenuItem>
+        <MenuItem value="16">16 per page</MenuItem>
+      </Select>
+
+      <ul className="flex gap-2 p-2 rounded-md border border-(--gray1) h-12">
+        <li className="size-8 flex items-center justify-center">
           <button
             type="button"
             onClick={() => setView("grid")}
-            className="p-3 cursor-pointer"
+            className={
+              " w-full h-full cursor-pointer " +
+              (listView === "grid" ? "text-black" : "text-(--gray4)")
+            }
           >
-            g
+            <ViewCompactIcon className="size-5" />
           </button>
         </li>
-        <li>
+        <li className={"size-8 flex items-center justify-center"}>
           <button
             type="button"
             onClick={() => setView("list")}
-            className="p-3 cursor-pointer"
+            className={
+              "w-full h-full cursor-pointer " +
+              (listView === "list" ? "text-black" : "text-(--gray4)")
+            }
           >
-            l
+            <ViewAgendaIcon className="size-5" />
           </button>
         </li>
       </ul>
