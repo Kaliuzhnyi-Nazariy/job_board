98c724a7a4ee8bf7bfd1fba8f8e5e599e720dc57
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-31 22:07:24 +0100

    style for settings nav

--- src/Components/candidateSettings/SettingsLayout.tsx
+++ src/Components/candidateSettings/SettingsLayout.tsx
@@ -1,23 +1,63 @@
-import { Link, Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router";
+
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+
+const basicStyles = "px-5 py-3 flex gap-2 items-center relative";
+const activeStyles = `text-(--primary5) before:content-[''] before:absolute before:bottom-0 before:left-0 before:w-full before:h-px before:bg-(--primary5)`;
 
 const SettingsLayout = () => {
   return (
-    <div className="flex flex-col">
-      <h1>Settings</h1>
-      <ul className="flex">
-        <li>
-          <Link to="personal">Personal</Link>
-        </li>
-        <li>
-          <Link to="profile">Profile</Link>
-        </li>
-        <li>
-          <Link to="social">Social Links</Link>
-        </li>
-        <li>
-          <Link to="account">Account Setting</Link>
-        </li>
-      </ul>
+    <div className="flex flex-col mt-12 ml-12">
+      <h5
+        style={{
+          marginBottom: 12,
+        }}
+      >
+        Settings
+      </h5>
+      <nav className="flex gap-3">
+        {/* <Link to="personal" className="px-5 py-3 flex gap-2 items-center"> */}
+        <NavLink
+          to="personal"
+          className={({ isActive }) =>
+            `${basicStyles} ${isActive && activeStyles}`
+          }
+        >
+          <PersonOutlineOutlinedIcon className="size-6" />
+          Personal
+        </NavLink>
+        {/* <Link to="profile" className="px-5 py-3 flex gap-2 items-center"> */}
+        <NavLink
+          to="profile"
+          className={({ isActive }) =>
+            `${basicStyles} ${isActive && activeStyles}`
+          }
+        >
+          <AccountCircleOutlinedIcon className="size-6" />
+          Profile
+        </NavLink>
+        <NavLink
+          to="social"
+          className={({ isActive }) =>
+            `${basicStyles} ${isActive && activeStyles}`
+          }
+        >
+          <LanguageOutlinedIcon className="size-6" />
+          Social Links
+        </NavLink>
+        <NavLink
+          to="account"
+          className={({ isActive }) =>
+            `${basicStyles} ${isActive && activeStyles}`
+          }
+        >
+          <SettingsOutlinedIcon className="size-6" />
+          Account Setting
+        </NavLink>
+      </nav>
 
       <Outlet />
     </div>
