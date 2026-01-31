283dde111c5eaebc9fb52d2df57898f2506baed9
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-01-31 18:23:39 +0100

    change link to navlink to simplify styles

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { Link, useLocation } from "react-router";
+import { NavLink } from "react-router";
 import { userRole } from "../../features/user/userSelector";
 
 import PhoneInTalkOutlinedIcon from "@mui/icons-material/PhoneInTalkOutlined";
@@ -7,88 +7,64 @@ import PhoneInTalkOutlinedIcon from "@mui/icons-material/PhoneInTalkOutlined";
 const Navbar = () => {
   const role = useSelector(userRole);
 
-  const { pathname } = useLocation();
-
   const homeLink = () => {
     return role == "employer" ? "/employer/home" : "/candidate/home";
   };
 
   const findLink = () => {
     if (role == "employer") {
-      return (
-        <Link to="/employer/candidates" className="h-full flex items-center">
-          Find Employers
-        </Link>
-      );
+      return { path: "/employer/candidates", text: "Find Employers" };
     } else {
-      return (
-        <Link to="/candidate/find-job" className="h-full flex items-center">
-          Find Job
-        </Link>
-      );
+      return { path: "/candidate/find-job", text: "Find Job" };
     }
   };
 
   const dashboardLink =
     role == "employer" ? "/employer/dashboard" : "/candidate/dashboard";
 
+  const navItem = "flex items-center border-b-2 transition-all duration-150";
+
+  const active = "text-(--primary5)";
+
+  const inactive =
+    "hover:text-(--primary4) hover:border-b-(--primary4) border-transparent";
+
   return (
     <header className="bg-(--gray50) px-75 h-12 flex justify-between">
-      <nav>
-        <ul className="flex gap-6 h-full">
-          {/* <li
-            className={`flex items-center hover:text-(--primary4) border-b-transparent border-b-2 hover:border-b-(--primary4) ${
-              pathname.includes("home") &&
-              "text-(--primary5) border-b-(--primary5)"
-            } transition-all duration-150`}
-          > */}
-          <li
-            className={`flex items-center border-b-transparent border-b-2 transition-all duration-150 hover:border-b-(--primary4) ${
-              pathname.includes("home")
-                ? "text-(--primary5)"
-                : "hover:text-(--primary4)"
-            }`}
-            style={
-              pathname.includes("home")
-                ? { borderBottomColor: "var(--primary5)" }
-                : { borderBottomColor: "transparent" }
-            }
-          >
-            <Link to={homeLink()} className="h-full flex items-center">
-              Home
-            </Link>
-          </li>
-          <li
-            className={`flex items-center border-b-transparent border-b-2 transition-all duration-150 hover:border-b-(--primary4) ${
-              pathname.includes("candidates") || pathname.includes("job")
-                ? "text-(--primary5)"
-                : "hover:text-(--primary4)"
-            }`}
-            style={
-              pathname.includes("candidates") || pathname.includes("job")
-                ? { borderBottomColor: "var(--primary5)" }
-                : { borderBottomColor: "transparent" }
-            }
-          >
-            {findLink()}
-          </li>
-          <li
-            className={`flex items-center border-b-transparent border-b-2 transition-all duration-150 hover:border-b-(--primary4) ${
-              pathname.includes("dashboard")
-                ? "text-(--primary5)"
-                : "hover:text-(--primary4)"
-            }`}
-            style={
-              pathname.includes("dashboard")
-                ? { borderBottomColor: "var(--primary5)" }
-                : { borderBottomColor: "transparent" }
-            }
-          >
-            <Link to={dashboardLink} className="h-full flex items-center">
-              Dashboard
-            </Link>
-          </li>
-        </ul>
+      <nav className="flex gap-6 h-full">
+        <NavLink
+          to={homeLink()}
+          className={({ isActive }) =>
+            `${navItem} ${isActive ? active : inactive}`
+          }
+          style={({ isActive }) =>
+            isActive ? { borderBottomColor: "var(--primary5)" } : undefined
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to={findLink().path}
+          className={({ isActive }) =>
+            `${navItem} ${isActive ? active : inactive}`
+          }
+          style={({ isActive }) =>
+            isActive ? { borderBottomColor: "var(--primary5)" } : undefined
+          }
+        >
+          {findLink().text}
+        </NavLink>
+        <NavLink
+          to={dashboardLink}
+          className={({ isActive }) =>
+            `${navItem} ${isActive ? active : inactive}`
+          }
+          style={({ isActive }) =>
+            isActive ? { borderBottomColor: "var(--primary5)" } : undefined
+          }
+        >
+          Dashboard
+        </NavLink>
       </nav>
       <span className="flex gap-2 items-center body_small_500">
         <PhoneInTalkOutlinedIcon />
--- src/pages/candidate/Dashboard.tsx
+++ src/pages/candidate/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet, useLocation } from "react-router";
+import { NavLink, Outlet } from "react-router";
 import Section from "../../Components/Section";
 
 import FilterNoneOutlinedIcon from "@mui/icons-material/FilterNoneOutlined";
@@ -10,15 +10,14 @@ import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 const Dashboard = () => {
   const baseURL = "/candidate/dashboard";
 
-  const { pathname } = useLocation();
-
-  // console.log("sdf/ddr5".endsWith("5"));
-
   const liStyle =
-    "flex gap-4 items-center px-5 py-2.5 transition-colors duration-100 w-full h-ful";
+    "flex gap-4 items-center px-5 py-2.5 transition-colors duration-100 w-full h-full";
+
+  const active = " bg-(--primary50) text-(--primary5)";
+  const inactive = " hover:text-black hover:bg-(--gray50)";
 
   return (
-    <Section extraStyles="flex gap-5">
+    <Section extraStyles="flex">
       <div className="outline outline-orange-600">
         <h4
           className="uppetcase body_xs_500 text-(--gray4) ml-5 w-72 "
@@ -27,54 +26,42 @@ const Dashboard = () => {
           candidate dashboard
         </h4>
         <nav className="body_small_500 text-(--gray5) ">
-          <Link
-            to={`${baseURL}`}
-            className={
-              liStyle +
-              (pathname.endsWith("dashboard")
-                ? " bg-(--primary50) text-(--primary5)"
-                : " hover:text-black hover:bg-(--gray50)")
+          <NavLink
+            to={baseURL}
+            end
+            className={({ isActive }) =>
+              `${liStyle} ${isActive ? active : inactive}`
             }
           >
             <FilterNoneOutlinedIcon className="size-6" /> Overview
-          </Link>
-          <Link
+          </NavLink>
+
+          <NavLink
             to={`${baseURL}/applied-jobs`}
-            className={
-              liStyle +
-              (pathname.endsWith("applied-jobs")
-                ? " bg-(--primary50) text-(--primary5)"
-                : " hover:text-black hover:bg-(--gray50)")
+            end
+            className={({ isActive }) =>
+              `${liStyle} ${isActive ? active : inactive}`
             }
           >
             <WorkOutlineOutlinedIcon className="size-6" /> Applied Jobs
-          </Link>
-          <Link
-            to={`${baseURL}/`}
-            className={liStyle + " hover:text-black hover:bg-(--gray50)"}
-          >
-            {" "}
+          </NavLink>
+
+          <NavLink to={baseURL} className={`${liStyle} ${inactive}`}>
             <BookmarkBorderOutlinedIcon className="size-6" /> Favorite Jobs
-          </Link>
-          <Link
-            to={`${baseURL}/`}
-            className={liStyle + " hover:text-black hover:bg-(--gray50)"}
-          >
-            {" "}
+          </NavLink>
+
+          <NavLink to={baseURL} className={`${liStyle} ${inactive}`}>
             <NotificationsActiveOutlinedIcon className="size-6" /> Job alert
-          </Link>
-          <Link
+          </NavLink>
+
+          <NavLink
             to={`${baseURL}/setting`}
-            className={
-              liStyle +
-              (pathname.includes("setting")
-                ? " bg-(--primary50) text-(--primary5)"
-                : " hover:text-black hover:bg-(--gray50)")
+            className={({ isActive }) =>
+              `${liStyle} ${isActive ? active : inactive}`
             }
           >
-            {" "}
             <SettingsOutlinedIcon className="size-6" /> Settings
-          </Link>
+          </NavLink>
         </nav>
       </div>
 
