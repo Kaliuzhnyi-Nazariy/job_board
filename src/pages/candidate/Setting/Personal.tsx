2c6d0e00a15680b0193e51df3bc24ae8fea774ba
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-02-01 20:35:30 +0100

    add styles

--- src/pages/candidate/Setting/Personal.tsx
+++ src/pages/candidate/Setting/Personal.tsx
@@ -10,6 +10,9 @@ import {
 import type { IUpdForm } from "../../../../features/candidate/interfaces";
 import { useAppDispatch } from "../../../../features/hooks/dispatchHook";
 import { getMe } from "../../../../features/user/userRequest";
+import { InputAdornment, TextField } from "@mui/material";
+
+import InsertLinkOutlinedIcon from "@mui/icons-material/InsertLinkOutlined";
 
 const Personal = () => {
   const userFullNameValue = useSelector(userFullName);
@@ -70,44 +73,125 @@ const Personal = () => {
     mutate({ ...data, id: userIdValue! });
   };
 
+  const labelStyles = "body_small mb-2";
+
   return (
-    <div>
-      <h2>Basic Information</h2>
-      <form className="grid grid-cols-2" onSubmit={handleSubmit(onSubmit)}>
+    <>
+      <h6 className="body_large_500">Basic Information</h6>
+      <form
+        className="grid grid-cols-[1fr_3fr] gap-x-12 mt-4.5"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div>
-          <label>Profile picture</label>
-          <div className="w-60 h-67 border border-dashed border-black/50"></div>
+          <label className={labelStyles}>Profile picture</label>
+          <div className="w-60 h-67 border border-dashed border-black/50 mt-2"></div>
         </div>
-        <div className="w-full grid grid-cols-2">
+        <div className="w-full grid grid-cols-2 gap-4.5">
           <div className="flex flex-col">
-            <label>Full name</label>
-            <input type="text" {...register("full_name")} />
-            <p>{errors.full_name?.message}</p>
+            <label className={labelStyles}>Full name</label>
+            <TextField
+              id="outlined"
+              {...register("full_name")}
+              sx={{
+                "& .css-16wblaj-MuiInputBase-input-MuiOutlinedInput-input": {
+                  padding: "12px 18px",
+                },
+                "& .css-18p5xg2-MuiNotchedOutlined-root-MuiOutlinedInput-notchedOutline":
+                  {
+                    borderColor: "#e4e5e8",
+                  },
+              }}
+            />
+            {errors.full_name && <p>{errors.full_name?.message}</p>}
           </div>
           <div className="flex flex-col">
-            <label>Title/headline</label>
-            <input type="text" {...register("speciality")} />
-            <p>{errors.speciality?.message}</p>
+            <label className={labelStyles}>Title/headline</label>
+            <TextField
+              id="outlined"
+              {...register("speciality")}
+              sx={{
+                "& .css-16wblaj-MuiInputBase-input-MuiOutlinedInput-input": {
+                  padding: "12px 18px",
+                },
+                "& .css-18p5xg2-MuiNotchedOutlined-root-MuiOutlinedInput-notchedOutline":
+                  {
+                    borderColor: "#e4e5e8",
+                  },
+              }}
+            />
+            {errors.speciality && <p>{errors.speciality?.message}</p>}
           </div>
           <div className="flex flex-col">
-            <label>Experience</label>
-            <input type="text" {...register("experience")} />
-            <p>{errors.experience?.message}</p>
+            <label className={labelStyles}>Experience</label>
+            <TextField
+              id="outlined"
+              {...register("experience")}
+              sx={{
+                "& .css-16wblaj-MuiInputBase-input-MuiOutlinedInput-input": {
+                  padding: "12px 18px",
+                },
+                "& .css-18p5xg2-MuiNotchedOutlined-root-MuiOutlinedInput-notchedOutline":
+                  {
+                    borderColor: "#e4e5e8",
+                  },
+              }}
+            />
+            {errors.experience && <p>{errors.experience?.message}</p>}
           </div>
           <div className="flex flex-col">
-            <label>Education</label>
-            <input type="text" {...register("education")} />
-            <p>{errors.education?.message}</p>
+            <label className={labelStyles}>Education</label>
+            {/* <input type="text" {...register("education")} /> */}
+            <TextField
+              id="outlined"
+              {...register("education")}
+              sx={{
+                "& .css-16wblaj-MuiInputBase-input-MuiOutlinedInput-input": {
+                  padding: "12px 18px",
+                },
+                "& .css-18p5xg2-MuiNotchedOutlined-root-MuiOutlinedInput-notchedOutline":
+                  {
+                    borderColor: "#e4e5e8",
+                  },
+              }}
+            />
+            {errors.education && <p>{errors.education?.message}</p>}
           </div>
           <div className="flex flex-col col-start-1 col-end-3">
-            <label>Personal website</label>
-            <input type="text" {...register("website")} />
-            <p>{errors.website?.message}</p>
+            <label className={labelStyles}>Personal website</label>
+            {/* <input type="text" {...register("website")} /> */}
+            <TextField
+              id="outlined"
+              {...register("website")}
+              sx={{
+                "& .css-16wblaj-MuiInputBase-input-MuiOutlinedInput-input": {
+                  padding: "12px 18px",
+                },
+                "& .css-18p5xg2-MuiNotchedOutlined-root-MuiOutlinedInput-notchedOutline":
+                  {
+                    borderColor: "#e4e5e8",
+                  },
+              }}
+              slotProps={{
+                input: {
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <InsertLinkOutlinedIcon
+                        className="rotate-135 font-light"
+                        sx={{ fill: "#0a65cc" }}
+                      />
+                    </InputAdornment>
+                  ),
+                },
+              }}
+            />
+            {errors.website && <p>{errors.website?.message}</p>}
           </div>
         </div>
-        <button className="col-start-2 justify-self-start">Save Changes</button>
+        <button className="col-start-2 justify-self-start mt-8 px-8 py-4 bg-(--primary5) text-white button rounded-sm hover:cursor-pointer hover:bg-(--primary6) trasnition-colors duration-150">
+          Save Changes
+        </button>
       </form>
-    </div>
+    </>
   );
 };
 
