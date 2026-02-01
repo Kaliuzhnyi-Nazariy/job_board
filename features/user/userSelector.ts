4c24e71f96de575ce2bcb81a6e5a35bb02e431a3
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-02-01 20:34:28 +0100

    add user email selector

--- features/user/userSelector.ts
+++ features/user/userSelector.ts
@@ -7,3 +7,4 @@ export const userError = (state: StoreState) => state.user.isError;
 export const username = (state: StoreState) => state.user.user?.username;
 export const userFullName = (state: StoreState) => state.user.user?.full_name;
 export const userId = (state: StoreState) => state.user.user?.id;
+export const userEmail = (state: StoreState) => state.user.user?.email;
