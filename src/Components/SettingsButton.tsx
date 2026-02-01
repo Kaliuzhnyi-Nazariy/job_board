2d6aa21d1954e118fc91eb20b1e150338e124ad5
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-02-01 20:35:02 +0100

    create save changes button for settings

---
+++ src/Components/SettingsButton.tsx
@@ -0,0 +1,10 @@
+const SettingsButton = () => {
+  // const SettingsButton = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <button className="px-6 py-3 text-white bg-(--primary5) rounded-sm button mt-8">
+      Save Changes
+    </button>
+  );
+};
+
+export default SettingsButton;
