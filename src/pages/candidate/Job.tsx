2f928c0c0d5bf77d8fdd41b228b50c143a621a88
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-02-02 21:00:01 +0100

    change styles

--- src/pages/candidate/Job.tsx
+++ src/pages/candidate/Job.tsx
@@ -3,7 +3,15 @@ import { useParams } from "react-router";
 import { getJob } from "../../../features/job/jobRequests";
 import ApplyModal from "../../Components/modals/ApplyModal";
 import React from "react";
-import { workTimeFormat } from "../../helpers/jobTimeFormat";
+import Section from "../../Components/Section";
+import WorkTimeBadge from "../../Components/WorkTimeBadge";
+import { dateFormat, workTimeFormat } from "../../helpers";
+
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import WorkOutlineOutlinedIcon from "@mui/icons-material/WorkOutlineOutlined";
+import WalletOutlinedIcon from "@mui/icons-material/WalletOutlined";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 
 const Job = () => {
   const { jobId } = useParams();
@@ -12,8 +20,6 @@ const Job = () => {
     queryKey: ["job", jobId],
     queryFn: () => getJob(jobId!),
   });
-
-  console.log(data);
 
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
@@ -36,68 +42,112 @@ const Job = () => {
     experience,
     location,
     salary,
+    created_at,
   } = data.jobs;
 
   return (
-    <div className="">
-      <div className="flex justify-between">
-        <div className="flex items-center">
-          <h3>{title}</h3>
-          <span className="px-3 py-0.75 bg-blue-400/20 text-blue-500">
-            {workTimeFormat(work_time)}
-          </span>
+    <Section>
+      <div className="flex justify-between py-8">
+        <div className="flex items-center gap-2">
+          <h5>{title}</h5>
+          <WorkTimeBadge jobTime={work_time} />
         </div>
-        <button type="button" onClick={handleOpen}>
-          Apply now
+        <button
+          type="button"
+          onClick={handleOpen}
+          className="px-16 py-4 bg-(--primary5) button text-white rounded-sm flex gap-3 items-center cursor-pointer hover:bg-(--primary6) transition-colors duration-150"
+        >
+          Apply now <ArrowRightAltIcon />
         </button>
       </div>
-      <div className="flex gap-12.5">
+      {/* <div className="flex gap-12.5"> */}
+      <div className="grid grid-cols-[55%_45%] gap-12.5">
         <div className="w-full">
-          <>
-            <h4>Job Description</h4>
-            <article>{description || "No description"}</article>
-          </>
-          <>
-            <h4>Responsibilities</h4>
-            <article>{responsibilities || "No responsibilities"}</article>
-          </>
+          <h6 className="body_large">Job Description</h6>
+          <article className="mt-4 text-(--gray6)">
+            {description || "No description"}
+          </article>
+
+          <p className="mt-8 body_large">Responsibilities</p>
+          <article className="mt-4 text-(--gray6)">
+            {responsibilities || "No responsibilities"}
+          </article>
         </div>
-        <div className="w-full">
-          <h5>Job overview</h5>
-          <ul className="grid grid-cols-2">
-            <li>
-              <div>
-                <div className="size-8"></div>
-                <span>EDUCATION</span>
-                <p>{education}</p>
+        <div className="w-full border border-(--primary50) rounded-lg p-8 max-w-134">
+          <p className="body_large ">Job overview</p>
+          <ul className="grid grid-cols-3 mt-6 gap-y-6">
+            <li className="flex flex-col gap-4">
+              <CalendarTodayOutlinedIcon
+                className="size-8"
+                sx={{ fill: "var(--primary5)" }}
+              />
+              <div className="">
+                <span className="text-[12px] text-(--gray5) uppercase">
+                  JOB POSTED:
+                </span>
+                <p className="body_small_500">
+                  {dateFormat(created_at, "dateOfBirth")}
+                </p>
               </div>
             </li>
-            <li>
-              <div>
-                <div className="size-8"></div>
-                <span>SALARY</span>
-                <p>{salary}</p>
+            <li className="flex flex-col gap-4">
+              <WorkOutlineOutlinedIcon
+                className="size-8"
+                sx={{ fill: "var(--primary5)" }}
+              />
+              <div className="">
+                <span className="text-[12px] text-(--gray5) uppercase">
+                  EDUCATION:
+                </span>
+                <p className="body_small_500">{education}</p>
               </div>
             </li>
-            <li>
-              <div>
-                <div className="size-8"></div>
-                <span>LOCATION</span>
-                <p>{location}</p>
+            <li className="flex flex-col gap-4">
+              <WalletOutlinedIcon
+                className="size-8"
+                sx={{ fill: "var(--primary5)" }}
+              />
+              <div className="">
+                <span className="text-[12px] text-(--gray5) uppercase">
+                  SALARY:
+                </span>
+                <p className="body_small_500">{salary}</p>
               </div>
             </li>
-            <li>
-              <div>
-                <div className="size-8"></div>
-                <span>JOB TYPE</span>
-                <p>{workTimeFormat(work_time)}</p>
+            <li className="flex flex-col gap-4">
+              <LocationOnIcon
+                className="size-8"
+                sx={{ fill: "var(--primary5)" }}
+              />
+              <div className="">
+                <span className="text-[12px] text-(--gray5) uppercase">
+                  Location:
+                </span>
+                <p className="body_small_500">{location}</p>
               </div>
             </li>
-            <li>
-              <div>
-                <div className="size-8"></div>
-                <span>EXPERIENCE</span>
-                <p>{experience}</p>
+            <li className="flex flex-col gap-4">
+              <WorkOutlineOutlinedIcon
+                className="size-8"
+                sx={{ fill: "var(--primary5)" }}
+              />
+              <div className="">
+                <span className="text-[12px] text-(--gray5) uppercase">
+                  job type:
+                </span>
+                <p className="body_small_500">{workTimeFormat(work_time)}</p>
+              </div>
+            </li>
+            <li className="flex flex-col gap-4">
+              <WorkOutlineOutlinedIcon
+                className="size-8"
+                sx={{ fill: "var(--primary5)" }}
+              />
+              <div className="">
+                <span className="text-[12px] text-(--gray5) uppercase">
+                  experience:
+                </span>
+                <p className="body_small_500">{experience}</p>
               </div>
             </li>
           </ul>
@@ -109,7 +159,7 @@ const Job = () => {
         jobTitle={title}
         jobId={jobId!}
       />
-    </div>
+    </Section>
   );
 };
 
