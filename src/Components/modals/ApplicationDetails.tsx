faf2ae25d05b271d3caf81fbd7b11fb215207bdd
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-02-02 20:58:55 +0100

    change styles

--- src/Components/modals/ApplicationDetails.tsx
+++ src/Components/modals/ApplicationDetails.tsx
@@ -3,8 +3,10 @@
 import { Box, Modal, Typography } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 import { getCandidateApplicationDetails } from "../../../features/application/applicationRequest";
-import { workTimeFormat } from "../../helpers/jobTimeFormat";
 import { dateFormat } from "../../helpers";
+import WorkTimeBadge from "../WorkTimeBadge";
+
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 
 const ApplicationDetails = ({
   open,
@@ -55,42 +57,48 @@ const ApplicationDetails = ({
           </>
         ) : (
           <>
-            <div className="flex gap-2 items-center">
-              <Typography variant="h6" component="h2">
-                Title: {data.title}
-              </Typography>
-              <Typography component="p">
-                {workTimeFormat(data.work_time)}
-              </Typography>
+            <div className="flex gap-4 items-center">
+              <h2 className="body_xl_500 uppercase">Job: {data.title}</h2>
+              <WorkTimeBadge jobTime={data.work_time} />
             </div>
-            <Typography component="p">Position: {data.position}</Typography>
-            <Typography component="p">Location: {data.location}</Typography>
-            <div>
-              <Typography component="h6">Description</Typography>
-              <Typography component="article">{data.description}</Typography>
+            <div className="flex gap-4 mt-2 body_medium text-(--gray2)">
+              <p className="">Position: {data.position}</p>
+              {" | "}
+              <span className=" flex gap-2 items-center">
+                <LocationOnIcon sx={{ height: 20, width: 20 }} />
+                <p>Location: {data.location}</p>
+              </span>
+              {" | "}
+
+              <p>Status: {data.status}</p>
+              {" | "}
+
+              <p>Applied: {dateFormat(data.applied_at)}</p>
             </div>
-            <div>
-              <Typography component="h6">Responsibilites</Typography>
-              <Typography component="article">
+            <div className="mt-4 w-full">
+              <h3 className="body_large_500 uppercase">Description</h3>
+              <article className="mt-6 body_medium">{data.description}</article>
+            </div>
+            <div className="mt-4 w-full">
+              <h3 className="body_large_500 uppercase">Responsibilites</h3>
+              <article className="mt-6 body_medium">
                 {data.responsibilities || "No responsibilities"}
-              </Typography>
+              </article>
             </div>
-            <hr />
+            <hr className="my-8" />
             <>
-              <Typography component="h6">Covering Letter</Typography>
-              <Typography component="article">
+              <h3 className="body_large_500 uppercase">Covering Letter</h3>
+              <article className="mt-6 body_medium">
                 {data.covering_letter || "No covering letter"}
-              </Typography>
+              </article>
             </>
-            <>
-              <Typography component="p">Status: {data.status}</Typography>
-            </>
-            <>
-              <Typography component="p">
-                Applied: {dateFormat(data.applied_at)}
-              </Typography>
-            </>
-            <button onClick={handleClose}>Close</button>
+
+            <button
+              onClick={handleClose}
+              className="px-6 py-3 rounded-sm bg-(--primary5) text-white button cursor-pointer flex justify-self-center mt-8"
+            >
+              Close
+            </button>
           </>
         )}
       </Box>
