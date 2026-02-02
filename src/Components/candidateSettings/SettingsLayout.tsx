738fc87bdb13e4c0e08a6cba6cbe549515edc97f
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-02-02 13:27:30 +0100

    implement section

--- src/Components/candidateSettings/SettingsLayout.tsx
+++ src/Components/candidateSettings/SettingsLayout.tsx
@@ -4,13 +4,15 @@ import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import DashboardSection from "../DashboardSection";
 
 const basicStyles = "px-5 py-3 flex gap-2 items-center relative";
 const activeStyles = `text-(--primary5) before:content-[''] before:absolute before:bottom-0 before:left-0 before:w-full before:h-px before:bg-(--primary5)`;
 
 const SettingsLayout = () => {
   return (
-    <div className="flex flex-col mt-12 ml-12">
+    <DashboardSection extraStyles="flex flex-col">
+      {/* <div className="flex flex-col mt-12 ml-12 max-w-246"> */}
       <h5
         style={{
           marginBottom: 12,
@@ -60,7 +62,7 @@ const SettingsLayout = () => {
       </nav>
 
       <Outlet />
-    </div>
+    </DashboardSection>
   );
 };
 
