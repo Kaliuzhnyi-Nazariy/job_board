e8c2d5f459e5ceb30489c6f143b6d0778a64f098
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-02-09 08:45:28 +0100

    implement new logic and styles

--- src/pages/employer/Candidates/FindACandidate.tsx
+++ src/pages/employer/Candidates/FindACandidate.tsx
@@ -1,45 +1,123 @@
 import { useQuery } from "@tanstack/react-query";
 import { getCandidates } from "../../../../features/candidate/candidatesRequsts";
 import { Link, useSearchParams } from "react-router";
-import { useEffect } from "react";
-import type { ICandidate } from "../../../../features/candidate/interfaces";
+import type {
+  // ICandidate,
+  ICandidateData,
+} from "../../../../features/candidate/interfaces";
+import Section from "../../../Components/Section";
+import Filters from "./Filters";
+
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import AttachMoneyOutlinedIcon from "@mui/icons-material/AttachMoneyOutlined";
+import ArrowRightAltOutlinedIcon from "@mui/icons-material/ArrowRightAltOutlined";
+import PaginationComponent from "../../../Components/Pagination";
 
 const FindACandidate = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  useEffect(() => {
-    return setSearchParams((prev) => {
-      const params = new URLSearchParams(prev);
-      params.set("location", "Poland");
-      return params;
-    });
-  }, []);
-
   const search = searchParams.get("search")?.trim() || "";
   const location = searchParams.get("location")?.trim() || "";
 
+  const orderParams = searchParams.get("order") || "oldest";
+  const page = Number(searchParams.get("page")) || 1;
+  const limit = Number(searchParams.get("limit")) || 12;
+  const listView = searchParams.get("list-view") || "grid";
+
+  const order = orderParams === "newest" ? "DESC" : "ASC";
+
   const { data, isLoading, isError } = useQuery({
-    queryKey: ["candidates", search, location],
-    queryFn: () => getCandidates(search, location),
+    queryKey: ["candidates", search, location, order],
+    queryFn: () => getCandidates(limit, search, location, order),
   });
 
   if (isLoading) return <p>Searching for candidates</p>;
 
   if (isError) return <p>Candidates not found!</p>;
 
-  return (
-    <div>
-      <h1>Find A Candidate</h1>
+  console.log({ data });
 
-      <>
-        {data && data.length > 0 ? (
-          <ul>
-            {data.map((c: ICandidate) => {
+  const pageAmount = Math.ceil(data.meta.total / data.meta.limit);
+
+  const listStyleView = "flex flex-col gap-6";
+  const gridListView = `grid ${
+    limit == 12 ? "grid-cols-3 gap-3 " : "grid-cols-4 gap-2 "
+  } grid-rows-4 `;
+
+  const photoStyles = `${
+    listView === "list"
+      ? "size-24"
+      : listView === "grid" && limit === 12
+      ? "size-24"
+      : "size-12"
+  }`;
+
+  const extraData = `${
+    listView === "list"
+      ? " gap-4 "
+      : listView == "grid" && limit == 12
+      ? " gap-2 "
+      : "flex-col gap-4"
+  }`;
+
+  return (
+    <Section>
+      {/* <h1>Find A Candidate</h1> */}
+      <Filters />
+      <div className="grid grid-cols-[424px_1fr] gap-6 mt-5 ">
+        <div className="w-106 h-302 bg-amber-500"></div>
+        {data && data.data.length > 0 ? (
+          <ul className={listView === "grid" ? gridListView : listStyleView}>
+            {data.data.map((c: ICandidateData) => {
               return (
-                <li key={c.id}>
-                  <Link to={`/employer/candidates/${c.id}`}>
-                    <h3>{c.full_name}</h3>
-                    <h4>{c.role}</h4>
+                <li
+                  key={c.id}
+                  className={`p-6 flex justify-between items-center border border-(--gray1) rounded-xl ${
+                    listView === "grid" && "flex-col"
+                  }`}
+                >
+                  <div
+                    className={`flex gap-5 ${
+                      listView === "grid" && "flex-col items-center"
+                    }`}
+                  >
+                    {/* <div className="flex flex-col gap-5"> */}
+                    {/* <div className="size-24 rounded-lg bg-(--gray5)"></div> */}
+                    <div
+                      className={`${photoStyles} rounded-lg bg-(--gray5)`}
+                    ></div>
+                    <div className={"flex flex-col gap-5"}>
+                      <div className="flex flex-col gap-1.5">
+                        <h3 className="body_large_500">{c.full_name}</h3>
+                        <h4 className="body_small text-(--gray6)">{c.role}</h4>
+
+                        {/* <div className="flex gap-4 mt-5"> */}
+                        <div className={`flex  mt-5 ${extraData}`}>
+                          <p className="flex items-center">
+                            <LocationOnIcon className="size-5.5 text-(--gray2)" />
+                            <span className="body_small text-(--gray6)">
+                              {c.location || "No location"}
+                            </span>
+                          </p>
+
+                          <p className="flex items-center">
+                            <AttachMoneyOutlinedIcon className="size-5.5 text-(--gray2)" />
+                            <span className="body_small text-(--gray6)">
+                              {c.experience || "No experience"}
+                            </span>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <Link
+                    to={`/employer/candidates/${c.id}`}
+                    className={`flex items-center gap-3 text-(--primary5) button bg-(--primary50) px-6 py-3 rounded-sm ${
+                      listView === "grid" && "mt-5"
+                    }`}
+                  >
+                    View Profile <ArrowRightAltOutlinedIcon />
                   </Link>
                 </li>
               );
@@ -48,8 +126,14 @@ const FindACandidate = () => {
         ) : (
           <p>No candidate found!</p>
         )}
-      </>
-    </div>
+      </div>
+
+      <PaginationComponent
+        page={page}
+        pageAmount={pageAmount}
+        setSearchParams={setSearchParams}
+      />
+    </Section>
   );
 };
 
--- src/pages/employer/Jobs/MyJobs.tsx
+++ src/pages/employer/Jobs/MyJobs.tsx
@@ -1,12 +1,31 @@
 import { useQuery } from "@tanstack/react-query";
 import { getMyJobs } from "../../../../features/job/jobRequests";
-import JobCard from "./JobCard";
-import type { IJob } from "../../../../features/job/interfaces";
+// import JobCard from "./JobCard";
+// import type { IJob } from "../../../../features/job/interfaces";
+import DashboardSection from "../../../Components/Dashboard/DashboardSection";
+// import OverviewRecentJobsList from "../../../Components/JobsListComponent";
+import { useSearchParams } from "react-router";
+// import { Pagination } from "@mui/material";
+import { useEffect } from "react";
+import JobsListComponent from "../../../Components/JobsListComponent";
+import PaginationComponent from "../../../Components/Pagination";
 
 const MyJobs = () => {
+  const [searchParams, setSearchParams] = useSearchParams({ page: "1" });
+
+  useEffect(() => {
+    setSearchParams((prev) => {
+      const params = new URLSearchParams(prev);
+      params.set("page", String(1));
+      return params;
+    });
+  }, []);
+
+  const page = Number(searchParams.get("page")) || 1;
+
   const { data, isLoading, isError } = useQuery({
-    queryKey: ["myjobs"],
-    queryFn: getMyJobs,
+    queryKey: ["myjobs", page],
+    queryFn: () => getMyJobs(page!),
     retry: false,
   });
 
@@ -14,32 +33,39 @@ const MyJobs = () => {
     return <p>Loading...</p>;
   }
 
+  console.log("data in my jobs: ", { data });
+
+  const pageAmount = Math.ceil(data.meta.allAmountOfJobs / data.meta.limit);
+  // const pageAmount = Math.ceil(data.allAmountOfJobs / 10);
+
+  // console.log(pageAmount);
+
   // in the future I will add ststuse and filter on that page
 
   return (
-    <div className="w-full px-5 mt-3">
-      <div className="flex justify-between">
-        <h1>My Jobs</h1>
-        {/* <p>Number of jobs: {data.job ? data.job.length : 0}</p> */}
+    <DashboardSection>
+      {/* <div className="flex justify-between"> */}
+      <div className="flex">
+        <h1 className="body_large_500">
+          My Jobs{" "}
+          <span className="text-(--gray4)">
+            ({data.job ? data.meta.allAmountOfJobs : 0})
+          </span>
+        </h1>{" "}
       </div>
-      {isLoading && <p>Loading...</p>}
+      <JobsListComponent
+        loadingRecentJobs={isLoading}
+        recentJobs={data.job}
+        title=""
+        isLink={false}
+      />
 
-      {isError && <p>Error occured!</p>}
-
-      {data && data.job && data.job.length > 0 ? (
-        <ul>
-          {data.job.map((j: IJob) => {
-            return (
-              <li key={j.id}>
-                <JobCard data={j} />
-              </li>
-            );
-          })}
-        </ul>
-      ) : (
-        <p>There are no your jobs!</p>
-      )}
-    </div>
+      <PaginationComponent
+        page={page}
+        pageAmount={pageAmount}
+        setSearchParams={setSearchParams}
+      />
+    </DashboardSection>
   );
 };
 
