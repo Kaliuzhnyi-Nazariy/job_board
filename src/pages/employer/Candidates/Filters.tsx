d6ea2621dc3f67a23346cbc57ab8243954e3dc4f
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-02-09 08:45:54 +0100

    filter button

---
+++ src/pages/employer/Candidates/Filters.tsx
@@ -0,0 +1,19 @@
+import TuneOutlinedIcon from "@mui/icons-material/TuneOutlined";
+import FilterListView from "../../../Components/FilterListView";
+// import { useSearchParams } from "react-router";
+
+const Filters = () => {
+  return (
+    <div className="flex justify-between items-center py-3">
+      <button
+        type="button"
+        className="flex gap-3 items-center px-6 py-3 bg-(--primary5) text-white button rounded-sm cursor-not-allowed hover:bg-(--primary6) transition-colors duration-150 "
+      >
+        <TuneOutlinedIcon className="size-6 rotate-90" /> Filter
+      </button>
+      <FilterListView />
+    </div>
+  );
+};
+
+export default Filters;
