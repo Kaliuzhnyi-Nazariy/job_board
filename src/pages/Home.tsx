a61e4c7221059beaf79abdf893e0888cf0953f1c
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-02-10 18:06:46 +0100

    home page all components implementation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,24 @@
 import Hero from "../Components/Home/Hero/Hero";
+import MostPopularVacancies from "../Components/Home/MostPopularVacancies/MostPopularVacancies";
+import Instruction from "../Components/Home/Instruction/Instruction";
+import PopularCategory from "../Components/Home/PopularCategory/PopularCategory";
+
+// import ClientsTestimonial from "../Components/Home/ClientsTestimonial/ClientsTestimonial";
+// import Join from "../Components/Home/Join/Join";
+// import Footer from "../Components/Home/Footer/Footer";
 
 const Home = () => {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <MostPopularVacancies />
+      <Instruction />
+      <PopularCategory />
+      {/*<ClientsTestimonial />
+      <Join />
+      <Footer /> */}
+    </>
+  );
 };
 
 export default Home;
