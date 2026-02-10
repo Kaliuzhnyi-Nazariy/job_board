ecb2e1fb2f96805907889291a59ab5ff0ccae00c
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-02-10 18:07:55 +0100

    create categories list and data

---
+++ src/extras/categories.ts
@@ -0,0 +1,14 @@
+export const theMostPopularVacancies: { title: string; amount: string }[] = [
+  { title: "Anesthesiologists", amount: "45,904" },
+  { title: "Surgeons", amount: "50,364" },
+  { title: "Obstetricians-Gynecologists", amount: "4,339" },
+  { title: "Orthodontists", amount: "20,079" },
+  { title: "Maxillofacial Surgeons", amount: "74,875" },
+  { title: "Software Developer", amount: "43,359" },
+  { title: "Psychiatrists", amount: "18,599" },
+  { title: "Data Scientist", amount: "28,200" },
+  { title: "Financial Manager", amount: "61,391" },
+  { title: "Management Analysis", amount: "93,046" },
+  { title: "IT Manager", amount: "50,963" },
+  { title: "Operations Research Analysis", amount: "16,627" },
+];
