995c1585fa026c2768ed83d4e3146ae2e9e1aeb2
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-02-11 08:29:24 +0100

    add slier package

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { Provider } from "react-redux";
 import store from "./store.ts";
+import "keen-slider/keen-slider.min.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -19,5 +20,5 @@ createRoot(document.getElementById("root")!).render(
         </BrowserRouter>
       </QueryClientProvider>
     </Provider>
-  </StrictMode>
+  </StrictMode>,
 );
