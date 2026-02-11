40e5a613c359172c2818b366181576a177f52737
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-02-11 08:29:58 +0100

    add slider into clients testimonial mock

--- src/Components/Home/ClientsTestimonial/ClientsTestimonial.tsx
+++ src/Components/Home/ClientsTestimonial/ClientsTestimonial.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import Section from "../../Section";
+import Slider from "./Slider/Slider";
 
 const ClientsTestimonial = () => {
-  return <div>ClientsTestimonial</div>;
+  return (
+    <Section extraStyles="py-20.5">
+      <h1>Clients Testimonial</h1>
+      <Slider />
+    </Section>
+  );
 };
 
 export default ClientsTestimonial;
---
+++ src/Components/Home/ClientsTestimonial/Slider/Slider.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import "./styles.css";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+
+// there are 5 messages
+
+export default function Slider() {
+  const [ref] = useKeenSlider<HTMLDivElement>({
+    slides: {
+      perView: 3,
+      spacing: 24,
+    },
+  });
+  return (
+    <div ref={ref} className="keen-slider">
+      <div className="keen-slider__slide number-slide1">1</div>
+      <div className="keen-slider__slide number-slide2">2</div>
+      <div className="keen-slider__slide number-slide3">3</div>
+      <div className="keen-slider__slide number-slide4">4</div>
+      <div className="keen-slider__slide number-slide5">5</div>
+      <div className="keen-slider__slide number-slide6">6</div>
+    </div>
+  );
+}
