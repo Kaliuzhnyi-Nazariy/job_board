16abdc7d5a1f5297f0df26e29a5e0c70bae69b0c
Nazarii Kaliuzhnyi <kaliuzhnyinazarii@gmail.com>
2026-02-15 10:50:32 +0100

    Update PictureLayout.tsx
    Remove image on viewport smaller than 1440px

--- src/pages/auth/PictureLayout.tsx
+++ src/pages/auth/PictureLayout.tsx
@@ -5,10 +5,10 @@ import BusinessOutlinedIcon from "@mui/icons-material/BusinessOutlined";
 
 const PictureLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="grid grid-cols-2 w-full h-screen justify-between">
+    <div className="min-[1440px]:grid min-[1440px]:grid-cols-2 w-full h-screen justify-between">
       {children}
 
-      <div className="relative h-full  ">
+      <div className="hidden min-[1440px]:block relative h-full  ">
         <div className="absolute w-full h-full bg-linear-[0deg,#041A3C_1%,#041a3c7c_100%,transparent] select-none"></div>
         <img
           src="/public/checkered_flag.jpg"
