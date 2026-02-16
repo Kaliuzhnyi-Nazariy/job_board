7e36b95aacd82c299d4bcb170479e95126c039a3
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-02-16 16:00:09 +0100

    make component more flexible to reuse

--- src/Components/Auth/ButtonAuth.tsx
+++ src/Components/Auth/ButtonAuth.tsx
@@ -7,16 +7,21 @@ import { userLoading } from "../../../features/user/userSelector";
 const ButtonAuth = ({
   isButtonEnabled,
   text,
+  extraStlyes,
 }: {
-  isButtonEnabled: boolean;
+  isButtonEnabled?: boolean;
   text: string;
+  extraStlyes?: string;
 }) => {
   const isLoading = useSelector(userLoading);
 
   return (
     <button
       type="submit"
-      className="py-4 bg-(--primary5) rounded-sm text-white button flex gap-3 items-center justify-center cursor-pointer hover:bg-(--primary6) disabled:bg-(--primary1) disabled:cursor-not-allowed transition-colors duration-150 w-full"
+      className={
+        "py-4 bg-(--primary5) rounded-sm text-white button flex gap-3 items-center justify-center cursor-pointer hover:bg-(--primary6) disabled:bg-(--primary1) disabled:cursor-not-allowed transition-colors duration-150 w-full " +
+        (extraStlyes ? extraStlyes : "")
+      }
       disabled={!isButtonEnabled || isLoading}
     >
       {isLoading ? (
