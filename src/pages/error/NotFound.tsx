501aea59397422e0ff22a48378a0bf7e8f740a58
Nazarii Kaliuzhnyi <kaliuzhnyinazarii@gmail.com>
2026-02-17 10:47:18 +0100

    Update NotFound.tsx

--- src/pages/error/NotFound.tsx
+++ src/pages/error/NotFound.tsx
@@ -1,5 +1,11 @@
+import {useNavigate} from "redux-router"
+
 const NotFound = () => {
-return <div>Page not found!</div>
+const navigate = useNavigate();
+return <div className="w-full h-screen grid justify-self-center">
+<h1>Page not found!</h1>
+<button type="button" onClick={()=> navigate("/")} className="w-60">Go home</button>
+</div>
 }
 
 export default NotFound
