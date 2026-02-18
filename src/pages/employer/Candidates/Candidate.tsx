76fc9c16830a2d9b28c4a29601ec20631c201a40
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-02-18 12:54:24 +0100

    add responsive design

--- src/pages/employer/Candidates/Candidate.tsx
+++ src/pages/employer/Candidates/Candidate.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { Link, useParams } from "react-router";
+import { useParams } from "react-router";
 import { getCandidate } from "../../../../features/candidate/candidatesRequsts";
 import type { FullDataCandidate } from "../../../../features/candidate/interfaces";
 // import { dateFormat } from "../../../helpers";
@@ -9,6 +9,7 @@ import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
 import GeneralData from "../../../Components/Candidate/GeneralData";
 import ContactData from "../../../Components/Candidate/ContactData";
+import LinkButton from "../../../Components/LinkButton";
 
 const Candidate = () => {
   const { candidateId } = useParams();
@@ -31,7 +32,7 @@ const Candidate = () => {
       {isLoading && <p>Loading...</p>}
       {data ? (
         <>
-          <div className="flex justify-between pt-12 pb-10">
+          <div className="flex flex-col min-[1024px]:flex-row justify-between pt-12 pb-10">
             <div className="flex gap-6 items-center">
               <div className="size-20 rounded-full bg-(--gray5)"></div>
               <div className="">
@@ -39,29 +40,30 @@ const Candidate = () => {
                 <small className="opacity-50">{data.speciality}</small>
               </div>
             </div>
-            <div className="flex gap-3 items-center">
-              {/* <button className="size-12 bg-blue-500/40"></button> */}
-              <Link
-                to={`mailto:${data.email}`}
-                className="border-2 border-(--primary5) rounded-sm px-6 py-3 flex gap-3 items-center h-12 button text-(--primary5) transition-colors duration-150 hover:bg-(--primary50) hover:text-(--primary6) hover:border-(--primary6) "
+            <div className="flex flex-col min-[1024px]:flex-row gap-3 items-center max-[1023px]:w-full mt-3 min-[1024px]:mt-0">
+              <LinkButton
+                link={`mailto:${data.email}`}
+                type="outlined"
+                extraStyles=" max-[1023px]:w-full text-center"
               >
                 <EmailOutlinedIcon />
                 <span>Send email</span>
-              </Link>
-              <button className="border border-transparent bg-(--primary5) text-white button flex gap-3 items-center px-6 py-3 rounded-sm h-12 cursor-pointer hover:bg-(--primary6) transition-colors duration-150">
+              </LinkButton>
+
+              <button className="border border-transparent bg-(--primary5) text-white button flex gap-3 items-center px-6 py-3 rounded-sm h-12 cursor-pointer hover:bg-(--primary6) transition-colors duration-150 max-[1023px]:w-full justify-center">
                 <ArrowCircleRightOutlinedIcon />
                 <span>Hire Candidate</span>
               </button>
             </div>
           </div>
-          <div className="grid grid-cols-[2fr_1fr] gap-18">
+          <div className="grid grid-cols-1 gap-3 min-[1440px]:grid-cols-[2fr_1fr] min-[1440px]:gap-18">
             <div className="">
               <h4 className="uppercase text-lg font-medium">Biography</h4>
               <article className="mt-6 body_medimum text-(--gray6)">
                 {data.biography || "no bio"}
               </article>
             </div>
-            <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-6 min-[640px]:gap-3 min-[640px]:grid min-[640px]:grid-cols-2 min-[1920px]:gap-6">
               <GeneralData
                 date_of_birth={data.date_of_birth}
                 gender={data.gender}
